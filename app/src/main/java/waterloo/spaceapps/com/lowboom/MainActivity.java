e9f2270251509d8b97e381d6fcd7e309bec88ddf
Peter Pogorski <ppogorsk@uwaterloo.ca>
2016-04-23 11:09:02 -0400

    Add radius to display boom

--- app/src/main/java/waterloo/spaceapps/com/lowboom/MainActivity.java
+++ app/src/main/java/waterloo/spaceapps/com/lowboom/MainActivity.java
@@ -1,5 +1,8 @@
 package waterloo.spaceapps.com.lowboom;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -11,7 +14,10 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -46,5 +52,34 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         mLatLng = new LatLng(43.451096, -80.498792);
         mMap.addMarker(new MarkerOptions().position(mLatLng));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mLatLng, 15));
+
+        int d = 500; // diameter
+        Bitmap bm = Bitmap.createBitmap(d, d, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bm);
+        Paint p = new Paint();
+        p.setColor(getResources().getColor(R.color.colorPrimary));
+        c.drawCircle(d/2, d/2, d/2, p);
+
+        Bitmap bm2 = Bitmap.createBitmap(d, d, Bitmap.Config.ARGB_8888);
+        Canvas c2 = new Canvas(bm2);
+        Paint p2 = new Paint();
+        p.setColor(getResources().getColor(R.color.colorAccent));
+        c2.drawCircle(d / 2, d / 2, d / 2, p2);
+        int radiusM = 1000;
+
+        // generate BitmapDescriptor from circle Bitmap
+        BitmapDescriptor bmD = BitmapDescriptorFactory.fromBitmap(bm);
+        BitmapDescriptor bmD2 = BitmapDescriptorFactory.fromBitmap(bm2);
+
+// mapView is the GoogleMap
+        mMap.addGroundOverlay(new GroundOverlayOptions().
+                image(bmD).
+                position(mLatLng, radiusM * 2, radiusM * 2).
+                transparency(0.4f));
+
+        mMap.addGroundOverlay(new GroundOverlayOptions().
+                image(bmD).
+                position(mLatLng, (radiusM + 500) * 2, (radiusM + 500) * 2).
+                transparency(0.4f));
     }
 }
