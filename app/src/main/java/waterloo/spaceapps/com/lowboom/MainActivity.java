f008b7cbc6d50696d3e66f61c55979d1e69a421c
Peter Pogorski <ppogorsk@uwaterloo.ca>
2016-04-23 10:52:04 -0400

    Set location to communitech

--- app/src/main/java/waterloo/spaceapps/com/lowboom/MainActivity.java
+++ app/src/main/java/waterloo/spaceapps/com/lowboom/MainActivity.java
@@ -1,18 +1,24 @@
 package waterloo.spaceapps.com.lowboom;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
+    private LatLng mLatLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +34,17 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        centerMapOnMyLocation();
+    }
 
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+    private void centerMapOnMyLocation() {
+
+        mMap.setMyLocationEnabled(true);
+
+        mLatLng = new LatLng(43.451096, -80.498792);
+        mMap.addMarker(new MarkerOptions().position(mLatLng));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mLatLng, 15));
     }
 }
